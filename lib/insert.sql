a8229f084305814f4a2358a42700c0ebca5c8c37
Jonathan Tan <you@example.com>
2019-09-25 23:14:04 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 15, "M", "brown", "calm", 0);
+INSERT INTO bears VALUES (2,"Rowdy", 9, "M", "brown", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 2, "F", "black", "Nice", 1);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 4, "M", "Green", "calm", 0);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "green", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 3, "M", "Black", "mean", 1);
+INSERT INTO bears VALUES (7,"Wendy", 14, "F", "green", "smart", 1);
+INSERT INTO bears VALUES (8,null, 25, "M", "black", "funny", 0);
