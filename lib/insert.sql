e17c1275cbe86e0b3d7bdad388cca88943358454
dan1mal <dreamer@panix.com>
2019-09-29 20:25:06 -0400

    edits

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES
+('Mr. Chocolate', 27, "male", "dark brown", "smooth", true),
+('Rowdy', 8, "male", "gray", "unstable", true),
+('Tabitha', 3, "female", "dark brown", "rambunctious", true),
+('Sergeant Brown', 22, "male", "brown-black", "aggressive", true),
+('Melissa', 7, "female", "honey brown", "sultry", true),
+('Grinch', 29, "male", "black", "salty", true),
+('Wendy', 20, "female", "dark brown", "smooth", true),
+(NULL, 12, "male", "black-brown", "evil", true);
+
