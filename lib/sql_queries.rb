3fc68eed74519dd3ae30a83949083233eb8b6566
github username <github email address>
2019-09-29 20:51:20 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -5,29 +5,55 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+    sql = <<-SQL
+  SELECT name FROM bears ORDER BY name ;
+  SQL
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+    sql = <<-SQL
+  SELECT name, age FROM bears WHERE alive=1 ORDER BY age ;
+  SQL
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+    sql = <<-SQL
+  SELECT name, age 
+  FROM bears
+  ORDER BY age DESC
+  LIMIT 1;
+  SQL
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+    sql = <<-SQL
+    SELECT name, age 
+    FROM bears
+    ORDER BY age ASC
+    LIMIT 1;
+  SQL
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+    sql = <<-SQL
+  SELECT color, COUNT(name)
+  FROM bears 
+  WHERE color="dark brown" ;
+  SQL
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+    sql = <<-SQL
+  SELECT COUNT(name)
+  FROM bears 
+  WHERE temperament="goofy" ;
+  SQL
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+    sql = <<-SQL
+  SELECT *
+  FROM bears 
+  WHERE bears.name IS NULL;
+  SQL
 end
